a24d85f4f0671e06d23844e8b9a4787ffce98412
Tal Muskal <tal@liquidapps.io>
2019-06-10 00:13:56 +0000

    sync

--- boxes/groups/eos-sdk/seed-eos/extensions/tools/eos/utils.js
+++ boxes/groups/eos-sdk/seed-eos/extensions/tools/eos/utils.js
@@ -21,7 +21,7 @@ let networks = {
   'kylin': {
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
     host: 'api.kylin.eosbeijing.one',
-    port: 8880,
+    port: 80,
     secured: false
   },
   'mainnet': {
