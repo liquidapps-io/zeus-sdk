54f06c55207aa0570ebcef71d035f1c86c48ce2e
Tal Muskal <tal@liquidapps.io>
2019-07-17 15:14:55 +0000

    sync

--- boxes/groups/services/sign-dapp-service/services/sign-dapp-service-node/index.js
+++ boxes/groups/services/sign-dapp-service/services/sign-dapp-service-node/index.js
@@ -1,37 +1,96 @@
 var { nodeFactory } = require('../dapp-services-node/generic-dapp-service-node');
-const { eosDSPGateway, paccount, resolveProviderPackage, deserialize, generateABI } = require('../dapp-services-node/common');
+const { eosDSPGateway, paccount, resolveProviderPackage, deserialize, generateABI, nodeosEndpoint } = require('../dapp-services-node/common');
 const { loadModels } = require("../../extensions/tools/models");
 var sha256 = require('js-sha256').sha256;
-const ecc = require('eosjs-ecc')
-const Fcbuffer = require('fcbuffer')
-const Eos = require('eosjs')
+const ecc = require('eosjs-ecc');
+const Fcbuffer = require('fcbuffer');
+const Eos = require('eosjs');
+const Web3 = require('web3');
+const os = require('os');
+const consts = require('./consts');
+const web3 = new Web3(web3Provider);
 
-
+// trx - ???
+// chain - the chain type (for sidechains e.g POA)
+// account - (I think) the multisig contract/account
+// which this key will be a party to
+// allowCreate - true if the key should already exist (a new key should NOT
+// be created)
 const getCreateKeypair = {
-  "eosio": (trx, chain, account, allowCreate) => {
+  "eosio": async (trx, chain, account, allowCreate) => {
+    const storagePath = getStoragePath(chain, account);
 
+    if (!allowCreate && !fs.existsSync(storagePath))
+      throw new Error(`key for account ${account} on chain ${chain} should exist`);    // storage somewhere
+
+    // storage somewhere
+    if (fs.existsSync(storagePath)) {
+      return JSON.parse(fs.readFileSync(storagePath));
+    }
+    const privateKey = await ecc.randomKey();
+    const publicKey = ecc.privateToPublic(privateKey);
+
+    // store the new key somewhere?
+    fs.writeFileSync(storagePath, JSON.stringify(newAccount));
+
+    return { privateKey, publicKey };
   },
   "binance": (trx, chain, account, allowCreate) => {
 
   },
   "ethereum": (trx, chain, account, allowCreate) => {
+    const storagePath = getStoragePath(chain, account);
 
+    if (!allowCreate && !fs.existsSync(storagePath))
+      throw new Error(`key for account ${account} on chain ${chain} should exist`);    // storage somewhere
+
+    if (fs.existsSync(storagePath)) {
+      return JSON.parse(fs.readFileSync(storagePath));
+    }
+
+    // otherwise we create, store, and return a new key
+    const newAccount = web3.eth.accounts.create();
+
+    // store the new key somewhere?
+    fs.writeFileSync(storagePath, JSON.stringify({
+       ...newAccount, publicKey: newAccount.address
+      })
+    );
+
+    return {
+      ...newAccount,
+      publicKey: newAccount.address // return extra field `publicKey` for consistency
+    }
   }
 }
 
-
+// trx - the transaction to sign and send
+// chain - the chain to send to (for sidechains e.g POA)
+// account - the account/contract to send the transaction to (usually multisig)
+// sigs - ???? I guess if using TSS then the DSP needs to know if the sig is complete
 const postFn = {
-  "eosio": (trx, chain, account, sigs) => {
-
+  "eosio": async (trx, chain, account, sigs) => {
+    const { privateKey, publicKey } = getCreateKeypair.eos(trx, chain, account, false);
+    const signedTx = await signFn.eos(trx, chain, account, { privateKey, publicKey });
+    const eos = getEos(privateKey, chain);
+    return eos.Api.transact
   },
   "binance": (trx, chain, account, sigs) => {
 
   },
-  "ethereum": (trx, chain, account, sigs) => {
-
+  "ethereum": async (trx, chain, account, sigs) => {
+    const { privateKey, publicKey } = getCreateKeypair.ethereum(trx, chain, account, false);
+    const signedTx = await signFn.ethereum(trx, chain, account, { privateKey, publicKey });
+    return web3.eth.sendSignedTransaction(signedTx);
   }
 }
 
+// trx - the transaction to sign and send
+// IMPORTANT - I'm assuming that trx is just the transaction DATA, and doesn't
+// include the nonce, gasPrice, other metadata etc. for ethereum
+// chain - the chain to send to (for sidechains e.g POA)
+// account - the account/contract to send the transaction to (usually multisig)
+// keypair - the keypair to sign with (I guess DSPs can have multiple signing keys)
 const signFn = {
   "eosio": (trx, chain, account, keypair) => {
 
@@ -39,14 +98,34 @@ const signFn = {
   "binance": (trx, chain, account, keypair) => {
 
   },
-  "ethereum": (trx, chain, account, keypair) => {
+  // note that `chain` here is usually referred to as chainId on ethereum
+  // which is a necessary transaction field (to prevent transactions
+  // on the mainnet being broadcasted to sidechains and vice versa - 
+  // similar to the functionality of ref_block_num/ref_block_prefix on EOS)
+  "ethereum": async (trx, chain, account, keypair) => {
+    // TODO: we need to take into account the nonce of pending transactions
+    // to not overwrite them - tricky business
+    const nonce = await web3.eth.getTransactionCount(keypair.publicKey);
+    const signedTx = await web3.eth.accounts.signTransaction({
+      nonce,
+      chainId: chain,
+      to: account,
+      data: trx,
+      value: 0,
+      gasPrice: consts.ethGasPrice,
+      gas: consts.ethGasLimit // We should ideally use
+      // web3.eth.estimateGas, but for that we need the abi of the contract
+      // we're sending the transaction to. hmmm....
+    }, keypair.privateKey);
 
+    return signedTx;
   }
 }
 
 const resolveTrxData = async(trxMeta) => {
   return trxMeta;
 }
+
 nodeFactory('sign', {
   sign: async({ event, rollback }, { id, trx, chain, chain_type, sigs, account, sigsRequired }) => {
     if (rollback) {
@@ -59,7 +138,7 @@ nodeFactory('sign', {
     }
     else {
       // get key from storage
-      var keypair = await getCreateKeypair(chain, chain_type, account);
+      var keypair = await getCreateKeypair[chain_type](chain, chain_type, account);
       // read transaction's action data, from ipfs or directly or raw from history
       var trxData = await resolveTrxData(trx);
 
@@ -87,13 +166,30 @@ nodeFactory('sign', {
       try {
         // todo: use auth service
         var { chain, chain_type, account } = body;
-        var keypair = await getCreateKeypair(chain, chain_type, account, true);
+        var keypair = await getCreateKeypair[chain_type](chain, chain_type, account, true);
         res.send(JSON.stringify({ public_key: keypair.publicKey }));
       }
       catch (e) {
         res.status(400);
         res.send(JSON.stringify({ error: e.toString() }));
       }
+    },
+    hello: async ({ body }, res) => {
+      res.send(JSON.stringify({ 'hello': 'world'}));
     }
   }
 });
+
+const getStoragePath = (chain, account) =>
+  `${os.homedir()}/keys/${chain}/${account}.json`;
+
+function getEos(privateKey, chainId) {
+  const config = {
+    expireInSeconds: 120,
+    sign: true,
+    chainId,
+    keyProvider: privateKey,
+    httpEndpoint: nodeosEndpoint
+  };
+  return new Eos(config);
+}
--- boxes/groups/services/sign-dapp-service/test/sign.spec.js
+++ boxes/groups/services/sign-dapp-service/test/sign.spec.js
@@ -4,40 +4,25 @@ require('babel-polyfill');
 const { assert } = require('chai'); // Using Assert style
 const { getCreateKeys } = require('../extensions/helpers/key-utils');
 const { getNetwork, getCreateAccount } = require('../extensions/tools/eos/utils');
-var Eos = require('eosjs');
+const Eos = require('eosjs');
 const getDefaultArgs = require('../extensions/helpers/getDefaultArgs');
 const fetch = require('node-fetch');
+const Web3 = require('web3');
+const contract = require('truffle-contract');
+const fs = require('fs');
+const path = require('path');
 
 const artifacts = require('../extensions/tools/eos/artifacts');
 const deployer = require('../extensions/tools/eos/deployer');
 const { genAllocateDAPPTokens } = require('../extensions/tools/eos/dapp-services');
 
+const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const web3 = new Web3(provider);
 
-function postData(url = ``, data = {}) {
-  // Default options are marked with *
-  return fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, cors, *same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        // "Content-Type": "application/json",
-        // "Content-Type": "application/x-www-form-urlencoded",
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrer: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    })
-    .then(async response => {
-      var text = await response.text();
-      var json = JSON.parse(text);
-      if (json.error)
-        throw new Error(json.error);
-      return json;
-    }); // parses response to JSON
-}
-var contractCode = 'signer';
-var ctrt = artifacts.require(`./${contractCode}/`);
+const dspUrl = 'http://localhost:13128';
+const contractCode = 'signer';
+const ctrt = artifacts.require(`./${contractCode}/`);
+
 describe(`Sign DAPP Service Test Contract`, () => {
   var testcontract;
   var testcontracta;
@@ -76,6 +61,21 @@ describe(`Sign DAPP Service Test Contract`, () => {
       }
     })();
   });
+
+  it('deploys multisig', done => {
+    (async() => {
+      try {
+        const multiSigContract = await deployEthMultiSig();
+        console.log('address', multiSigContract.address);
+        // generate keys
+        // sign 
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    })();
+  })
 
   it.skip('sign call - single sig', done => {
     (async() => {
@@ -129,3 +129,64 @@ describe(`Sign DAPP Service Test Contract`, () => {
   });
 
 });
+
+function postData(url = ``, data = {}) {
+  // Default options are marked with *
+  return fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        // "Content-Type": "application/json",
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    })
+    .then(async response => {
+      var text = await response.text();
+      var json = JSON.parse(text);
+      if (json.error)
+        throw new Error(json.error);
+      return json;
+    }); // parses response to JSON
+}
+
+// deploys multisig from accounts[0] with accounts[1] as signer
+async function deployEthMultiSig() {
+  const multiSigAbi = JSON.parse(fs.readFileSync(path.resolve('./test/eth-build/multisigAbi.json')));
+  const multiSigBin = fs.readFileSync(path.resolve('./test/eth-build/multisig.bin'), 'utf8');
+
+  const availableAccounts = await web3.eth.getAccounts();
+  const masterAccount = availableAccounts[0];
+  const signerAccount = availableAccounts[1];
+
+  const multiSigContract = contract({
+    abi: multiSigAbi,
+    unlinked_binary: multiSigBin
+  });
+
+  multiSigContract.setProvider(web3.currentProvider);
+
+  const deployedContract = await multiSigContract.new([signerAccount], '1', {
+    from: masterAccount,
+    gas: '5000000'
+  });
+  return deployedContract; 
+}
+
+async function createDspKey(chain, chain_type, account) {
+  const dspPublicKey = await postData(`${dspUrl}/v1/dsp/genkey`, { chain, chain_type, account });
+  // fund the new account?
+  if (chain == 'ethereum') {
+    let fundingAccount = (await web3.eth.getAccounts())[1];
+    let fundingAmount = '1000000000000000000'; // 1 ETH
+    await web3.eth.sendTransaction({
+      from: fundingAccount,
+      to: dspPublicKey,
+      value: fundingAmount
+    });
+  }
+}
