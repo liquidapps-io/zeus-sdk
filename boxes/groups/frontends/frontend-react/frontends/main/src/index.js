a34d04b87c205813f4b142a6895b3bfd23ff3f5d
Tal Muskal <tal@liquidapps.io>
2019-11-19 19:37:06 +0000

    sync

--- boxes/groups/frontends/frontend-react/frontends/main/src/index.js
+++ boxes/groups/frontends/frontend-react/frontends/main/src/index.js
@@ -2,65 +2,87 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages/index';
 
-import Eos from 'eosjs';
+import ScatterJS from '@scatterjs/core';
+import ScatterEOS from '@scatterjs/eosjs2';
+import {JsonRpc, Api} from 'eosjs';
 
-// eosio endpoint
-// var debug = false;
-// var endpoint = "https://api.eosrio.io";
-// if(debug)
-//   endpoint = "http://54.186.222.85:8888";
+ScatterJS.plugins( new ScatterEOS() );
 
-// if(window.location.host ===  "heartbeat.liquideos.com"){
-//   endpoint = "http://api.eosrio.io";
-// }
-
-// if(window.location.host === "jungle-heartbeat.liquideos.com"){
-//   endpoint = "http://dev.cryptolions.io:38888";
-// }
-
-const network = {
-  blockchain: 'eos',
-  protocol: 'https',
-  host: 'nodes.get-scatter.com',
-  port: 443,
-  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
-};
-var jungle = false;
-var local = true;
+const mainnet = false;
+const jungle = false;
+const kylin = true;
+const local = false;
+let chainId, host, port, protocol;
+const blockchain = 'eos';
+if (mainnet) {
+  chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
+  port = 443;
+  host = 'nodes.get-scatter.com';
+  protocol = 'https';
+}
 if (jungle) {
-  network.chainId = '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca';
-  network.port = 443;
-  network.host = 'jungle.eosio.cr';
-  network.protocol = 'https';
+  chainId = '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca';
+  port = 443;
+  host = 'jungle.eosio.cr';
+  protocol = 'https';
 }
-
+if (kylin) {
+  chainId = '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191';
+  port = 443;
+  host = 'api.kylin.alohaeos.com';
+  protocol = 'https';
+}
 if (local) {
-  network.chainId = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f';
-  network.port = 8888;
-  network.host = '54.186.222.85';
-  network.protocol = 'http';
+  chainId = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f';
+  port = 8888;
+  host = 'localhost';
+  protocol = 'http';
 }
 
-// var ScatterJS = window.ScatterJS;
-async function init () {
-  // window.scatter = ScatterJS.scatter;
-  const scatter = window.scatter;
-  const connected = await scatter.connect('AirdropGrabber');
-  if (!connected) {
-    // User does not have Scatter Desktop or Classic installed.
-    return false;
-  }
-  const requiredFields = { accounts: [network] };
-  // Use `scatter` normally now.
+function init () {
+    const network = ScatterJS.Network.fromJson({
+        blockchain,
+        chainId,
+        host,
+        port,
+        protocol
+    });
+    const rpc = new JsonRpc(network.fullhost());
 
-  await scatter.getIdentity(requiredFields);
-  const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-  // You can pass in any additional options you want into the eosjs reference.
-  const eosOptions = { expireInSeconds: 600 };
-  window.accountName = account.name;
-  // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
-  const eos = scatter.eos(network, Eos, eosOptions, 'http');
-  window.eos = eos;
+    ScatterJS.connect('dapp', {network}).then(connected => {
+        if(!connected) return console.error('no scatter');
+
+        const eos = ScatterJS.eos(network, Api, {rpc});
+
+        ScatterJS.login().then(id => {
+            if(!id) return console.error('no identity');
+            const account = ScatterJS.account('eos');
+
+            eos.transact({
+                actions: [{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: [{
+                        actor: account.name,
+                        permission: account.authority,
+                    }],
+                    data: {
+                        from: account.name,
+                        to: 'safetransfer',
+                        quantity: '0.0001 EOS',
+                        memo: account.name,
+                    },
+                }]
+            }, {
+                blocksBehind: 3,
+                expireSeconds: 30,
+            }).then(res => {
+                console.log('sent: ', res);
+            }).catch(err => {
+                console.error('error: ', err);
+            });
+        });
+    });
 }
 init();
 ReactDOM.render(<Index />, document.getElementById('root'));
