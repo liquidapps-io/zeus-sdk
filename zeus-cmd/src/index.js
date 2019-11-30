7791c6cce275d782dda14cd313b238f77986a499
prc <prcolaco@gmail.com>
2019-11-30 03:32:37 +0000

    Fix rc file parsing for sub commands

--- zeus-cmd/src/index.js
+++ zeus-cmd/src/index.js
@@ -25,21 +25,31 @@ var currentYargs = require('yargs') // eslint-disable-line
     describe: 'ignore rc file',
     default: false
   })
+  .middleware(argv => {
+    // Load rc file if any or report rc ignore to user
+    if (!argv['rc-ignore'] && fs.existsSync(argv['rc-file'])) {
+      console.log(`loading options from rc file ${argv['rc-file']}`);
+      const rc = JSON.parse(fs.readFileSync(argv['rc-file']));
+      var config = { ...rc.global };
+      var other = rc;
+      argv['_'].map(cmd => {
+        if (config[cmd]) delete config[cmd];
+        other = (other && cmd in other) ? other[cmd] : undefined;
+        if (other) config = { ...config, ...other };
+      });
+      argv = { ...argv, ...config };
+    } else if (argv['rc-ignore']) {
+      console.log('ignoring rc file');
+    }
+    console.log(argv);
+    return argv;
+  }, true)
   .option('verbose', {
     alias: 'v',
     default: false
   }).commandDir('cmds').help('help')
   .showHelpOnFail(false, 'whoops, something went wrong! run with --help')
   .help('h').alias('h', 'help').demandCommand().completion().strict();
-
-// Load rc file if any or report rc ignore to user
-if (!currentYargs.argv.rcIgnore && fs.existsSync(currentYargs.argv.rcFile)) {
-  console.log(`loading options from rc file ${currentYargs.argv.rcFile}`);
-  const config = JSON.parse(fs.readFileSync(currentYargs.argv.rcFile));
-  currentYargs = currentYargs.config(config);
-} else if (currentYargs.argv.rcIgnore) {
-  console.log('ignoring rc file');
-}
 
 var extPath = path.join(path.resolve('.'), 'extensions/commands');
 if (fs.existsSync(extPath) && global.enableExt) { currentYargs = currentYargs.commandDir(extPath); }
