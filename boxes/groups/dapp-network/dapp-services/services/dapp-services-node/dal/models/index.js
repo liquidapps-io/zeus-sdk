98a665614e27421e01becdc1e1937e0bd638061a
Tal Muskal <tal@liquidapps.io>
2022-03-30 16:18:40 +0000

    sync

--- boxes/groups/dapp-network/dapp-services/services/dapp-services-node/dal/models/index.js
+++ boxes/groups/dapp-network/dapp-services/services/dapp-services-node/dal/models/index.js
@@ -38,7 +38,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = addTimeoutProxy(model, dbTimeout, model.name);
   });
 
