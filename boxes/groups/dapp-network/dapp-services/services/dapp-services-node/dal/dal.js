4627f28dfa7502c8a62993d2f8967c15da3bfb0a
Tal Muskal <tal@liquidapps.io>
2022-03-31 18:10:40 +0000

    sync

--- boxes/groups/dapp-network/dapp-services/services/dapp-services-node/dal/dal.js
+++ boxes/groups/dapp-network/dapp-services/services/dapp-services-node/dal/dal.js
@@ -6,10 +6,14 @@ const logger = requireBox('log-extensions/helpers/logger');
 let synced = false;
 
 async function getServiceRequest(key) {
-  await sync();
-  return db.ServiceRequest.findOne({
-    where: { key }
-  });
+  try {
+    await sync();
+    return db.ServiceRequest.findOne({
+      where: { key }
+    });
+  } catch(e) {
+    logger.error(e);
+  }
 }
 
 // pass in new fields to be set
@@ -79,86 +83,118 @@ async function createCronInterval(key, timer, payload, seconds, event) {
 }
 
 async function removeCronInterval(key) {
-  await sync();
-  logger.debug(`db.CronInterval.destroy`)
-  var res = await db.CronInterval.destroy({
-    where: { key }
-  });
-  return res;
+  try {
+    await sync();
+    logger.debug(`db.CronInterval.destroy`)
+    var res = await db.CronInterval.destroy({
+      where: { key }
+    });
+    return res;
+  } catch(e) {
+    logger.error(e);
+  }
 }
 
 async function fetchCronInterval(key) {
-  await sync();
-  logger.debug(`db.CronInterval.findOne`)
-  var res = await db.CronInterval.findOne({ where: { key } });
-  if (!res) {
-    return;
-  }
-  return res;
+  try {
+    await sync();
+    logger.debug(`db.CronInterval.findOne`)
+    var res = await db.CronInterval.findOne({ where: { key } });
+    if (!res) {
+      return;
+    }
+    return res;
+  } catch(e) {
+    logger.error(e);
+}
 }
 
 async function fetchAllCronInterval() {
-  await sync();
-  logger.debug(`db.CronInterval.findAll`)
-  var res = await db.CronInterval.findAll();
-  if (!res) {
-    return;
+  try {
+    await sync();
+    logger.debug(`db.CronInterval.findAll`)
+    var res = await db.CronInterval.findAll();
+    if (!res) {
+      return;
+    }
+    return res;
+  } catch(e) {
+    logger.error(e);
   }
-  return res;
 }
 
 // settings table contains singleton with key 'settings'
 async function getSettings() {
-  await sync();
-  return db.Settings.findOne({
-    where: { key: 'settings' }
-  });
+  try {
+    await sync();
+    return db.Settings.findOne({
+      where: { key: 'settings' }
+    });
+  } catch(e) {
+    logger.error(e);
+  }
 }
 
 async function updateSettings(data) {
-  await sync();
-  const currentSettings = await getSettings();
-  if (currentSettings) {
-    data = { ...currentSettings.data, ...data };
-    await db.Settings.update({ data }, { where: { key: 'settings' }});
-  } else {
-    await db.Settings.create({ key: 'settings', data });
+  try {
+    await sync();
+    const currentSettings = await getSettings();
+    if (currentSettings) {
+      data = { ...currentSettings.data, ...data };
+      await db.Settings.update({ data }, { where: { key: 'settings' }});
+    } else {
+      await db.Settings.create({ key: 'settings', data });
+    }
+  } catch(e) {
+    logger.error(e);
   }
 }
 
 async function fetchNonce(chain) {
-  logger.debug('fetchNonce')
-  await sync();
-  logger.debug('db.Nonce.findOne')
-  var res = await db.Nonce.findOne({ where: { key: chain } });
-  if (!res) {
-    return;
+  try {
+    logger.debug('fetchNonce')
+    await sync();
+    logger.debug('db.Nonce.findOne')
+    var res = await db.Nonce.findOne({ where: { key: chain } });
+    if (!res) {
+      return;
+    }
+    return res;
+  } catch(e) {
+    logger.error(e);
   }
-  return res;
 }
 
 async function updateNonce(data, chain) {
-  logger.debug('fetchNonce')
-  await sync();
-  logger.debug('currentNonce')
-  const currentNonce = await fetchNonce(chain);
-  if (currentNonce) {
-    data = { ...currentNonce.data, ...data };
-    logger.debug('db.Nonce.update')
-    await db.Nonce.update({ data }, { where: { key: chain }});
-  } else {
-    logger.debug('db.Nonce.create')
-    await db.Nonce.create({ key: chain, data });
+  try {
+    logger.debug('fetchNonce')
+    await sync();
+    logger.debug('currentNonce')
+    const currentNonce = await fetchNonce(chain);
+    if (currentNonce) {
+      data = { ...currentNonce.data, ...data };
+      logger.debug('db.Nonce.update')
+      await db.Nonce.update({ data }, { where: { key: chain }});
+    } else {
+      logger.debug('db.Nonce.create')
+      await db.Nonce.create({ key: chain, data });
+    }
+  } catch(e) {
+    logger.error(e);
   }
 }
 
 async function sync() {
-  logger.debug('syncing')
-  if (synced)
-    return;
+  try {
+    logger.debug('syncing')
+    if (synced)
+      return;
 
-  await db.sequelize.sync();
-  synced = true;
+    await db.sequelize.sync();
+    synced = true;
+  } catch(e) {
+    logger.error(e);
+  }
 }
 
 module.exports = {
